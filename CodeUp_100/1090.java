35fd115fa549ddf7896192954220a8730cf6a26e
whitetigerlouis <82086525+whitetigerlouis@users.noreply.github.com>
2021-10-25 14:16:31 +0900

    Create 1090.java
    1090 : [기초-종합] 수 나열하기2
    https://codeup.kr/problem.php?id=1090

---
+++ CodeUp_100/1090.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt(); // 시작값
+        int r = in.nextInt(); // 등비의 값
+        int n = in.nextInt(); // 몇 번째 수
+        int sum = a;
+        for (int i = 2; i <= n; i++) {
+            sum *= r;
+        }
+        System.out.println(sum);
+    }
+}
