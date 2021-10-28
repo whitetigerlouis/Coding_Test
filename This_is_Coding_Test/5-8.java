3870b3af139317112d0f625370f763bcf2a9f0c4
whitetigerlouis <82086525+whitetigerlouis@users.noreply.github.com>
2021-10-28 22:50:40 +0900

    Create 5-8.java
    05. DFS의 기본 알고리즘 구현
    - 노드 구성을 ArrayList<ArrayList<Integer>>()를 사용한다.

---
+++ This_is_Coding_Test/5-8.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+public class Main {
+    public static boolean[] visited = new boolean[9];
+    // 그래프를 표현하기 위해 ArrayList의 ArrayList를 사용했다.
+    public static ArrayList<ArrayList<Integer>> graph = new ArrayList<ArrayList<Integer>>();
+
+    // DFS
+    public static void dfs(int x) {
+        // 현재 노드를 방문 처리
+        visited[x] = true;
+        System.out.print(x + " ");
+        // 현재 노드와 연결된 다른 노드를 재귀적으로 방문
+        for (int i = 0; i < graph.get(x).size(); i++) {
+            int y = graph.get(x).get(i); // stack 구조니까 get(i)를 하면 나중에 들어간 순서대로 꺼낸다.
+            // y 노드를 방문하지 않았다면 dfs(y) 재귀호출
+            if (visited[y] == false) dfs(y);
+        }
+    }
+    public static void main(String[] args) {
+        // 그래프 초기화
+        for (int i = 0; i < 9; i++) {
+            graph.add(new ArrayList<Integer>());
+        }
+
+        // 노드 1에 연결된 노드 구성
+        graph.get(1).add(2);
+        graph.get(1).add(3);
+        graph.get(1).add(8);
+
+        // 노드 2에 연결된 노드 구성
+        graph.get(2).add(1);
+        graph.get(2).add(7);
+
+        // 노드 3에 연결된 노드 정보 구성
+        graph.get(3).add(1);
+        graph.get(3).add(4);
+        graph.get(3).add(5);
+
+        // 노드 4에 연결된 노드 정보 저장
+        graph.get(4).add(3);
+        graph.get(4).add(5);
+
+        // 노드 5에 연결된 노드 정보 저장
+        graph.get(5).add(3);
+        graph.get(5).add(4);
+
+        // 노드 6에 연결된 노드 정보 저장
+        graph.get(6).add(7);
+
+        // 노드 7에 연결된 노드 정보 저장
+        graph.get(7).add(2);
+        graph.get(7).add(6);
+        graph.get(7).add(8);
+
+        // 노드 8에 연결된 노드 정보 저장
+        graph.get(8).add(1);
+        graph.get(8).add(7);
+
+        dfs(1);
+    }
+}
