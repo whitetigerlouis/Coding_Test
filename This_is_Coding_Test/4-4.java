166150ff827160dd933cbc8238aa3917f4a5921b
whitetigerlouis <82086525+whitetigerlouis@users.noreply.github.com>
2021-10-28 17:07:58 +0900

    Update 4-4.java
    while 문 마지막에 turn_time=0을 안해줘서 무한루프에 빠짐.

--- This_is_Coding_Test/4-4.java
+++ This_is_Coding_Test/4-4.java
@@ -7,8 +7,6 @@ import java.util.*;
     안 가본 칸이 없으면 회전만 가능
     4. 네 방향 모두 가본 칸이거나 바다로 되어 있으면, 보는 방향을 유지한 채 한 칸 뒤로 간다.
     이때 뒤가 바다이면 움직임을 멈춘다. 
-
-
 */
 public class Main {
     public static int n, m, x, y, direction;
@@ -59,19 +57,22 @@ public class Main {
             turn_left();
             int nx = x + dx[direction];
             int ny = y + dy[direction];
+            //System.out.println("nx = " + nx + ", ny = " + ny);
             
-            // 2. 회전 후 정면에 가보지 않은 유지가 있으면 이동
+            // 2. 회전 후 정면에 가보지 않은 위치가 있으면 이동
             if (visited[nx][ny] == 0 && map[nx][ny] == 0) {
                 visited[nx][ny] = 1; // 방문처리
                 x = nx; // 이동 후 위치로 재설정
                 y = ny;
                 cnt += 1; // 방문횟수 1 증가
                 turn_time = 0; // 회전횟수 초기화
+                //System.out.println("[FOUND] x = " + x + ", y = " + y);
                 continue; // 이동 했으니 다시 step 1으로
             } else {
                 // 3. 회전 후 정면에 가보지 않은 칸이 없거나 바다인 경우
                 turn_time += 1; // 회전횟수 1 증가
             }
+            //System.out.println("turn_time = " + turn_time);
 
             // 4. 다 회전 했으나 네 방향 모두 이동 못하는 경우
             if (turn_time == 4) {
@@ -83,8 +84,11 @@ public class Main {
                     y = ny;
                 } else {
                     // 뒤도 바다인 경우
+                    //System.out.println("break");
                     break;
                 }
+                // 0으로 초기화하지 않으면 무한루프
+                turn_time = 0;
             }
         }
         System.out.println(cnt);
