13bd6aa620fa791968c197567b1ba7da87e2ddb1
whitetigerlouis <82086525+whitetigerlouis@users.noreply.github.com>
2022-06-11 09:12:36 +0900

    Create 05.java

---
+++ Inflearn_Java_Algorithm/06_Sorting_Searching/05.java
@@ -0,0 +1,62 @@
+ /*
+5. 중복 확인
+설명
+현수네 반에는 N명의 학생들이 있습니다.
+선생님은 반 학생들에게 1부터 10,000,000까지의 자연수 중에서 각자가 좋아하는 숫자 하나 적어 내라고 했습니다.
+만약 N명의 학생들이 적어낸 숫자 중 중복된 숫자가 존재하면 D(duplication)를 출력하고,
+N명이 모두 각자 다른 숫자를 적어냈다면 U(unique)를 출력하는 프로그램을 작성하세요.
+
+입력
+첫 번째 줄에 자연수 N(5<=N<=100,000)이 주어진다.
+두 번째 줄에 학생들이 적어 낸 N개의 자연수가 입력된다.
+
+출력
+첫 번째 줄에 D 또는 U를 출력한다.
+
+예시 입력 1 
+8
+20 25 52 30 39 33 43 33
+
+예시 출력 1
+D
+*/
+
+import java.util.*;
+
+
+public class Main {
+    int n;
+    int[] A;
+    public void inputData() {
+        Scanner in = new Scanner(System.in);
+        n = in.nextInt();
+        A = new int[n];
+        for (int i=0; i<n; i++) {
+            A[i] = in.nextInt();
+        }
+        in.close();
+    }
+
+  /*
+    1. 정렬 사용(HashMap 사용하면 더 간단함)
+    2. 같은 수를 만나면 D 리턴
+    3. 마지막까지 정렬이 진행되면 U 리턴
+  */
+    public char solve() {
+        char answer = 'U';
+        Arrays.sort(A);
+        for (int i=0; i<n-1; i++) {
+            if (A[i] == A[i+1])
+                return 'D';
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main m = new Main();
+        m.inputData();
+        //for (int x : m.solve())
+        //    System.out.print(x + " ");
+        System.out.println(m.solve());
+    }
+}
