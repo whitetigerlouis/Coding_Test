e6590b97e3cc3215291086b0f3b92194ae5fe588
whitetigerlouis <82086525+whitetigerlouis@users.noreply.github.com>
2022-06-12 16:37:43 +0900

    Create 05.java

---
+++ Inflearn_Java_Algorithm/07_Recursive_Tree_Graph/05.java
@@ -0,0 +1,44 @@
+ /*
+1. 재귀함수(스택프레임)
+*/
+
+import java.util.*;
+
+class Node {
+    int data;
+    Node lt, rt;
+    public Node(int val) {
+        data = val;
+        lt=rt=null;
+    }
+}
+
+public class Main {
+    Node root;
+    public void DFS(Node root){
+        if(root == null) return;// 말단에 연결된 노드가 없으면 리턴
+        else {
+            //System.out.print(root.data+" ");
+            DFS(root.lt);
+            //System.out.print(root.data+" ");
+            DFS(root.rt);
+            System.out.print(root.data+" ");
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+        Main tree = new Main();
+        tree.root = new Node(1);
+        tree.root.lt = new Node(2);
+        tree.root.rt = new Node(3);
+        tree.root.lt.lt = new Node(4);
+        tree.root.lt.rt = new Node(5);
+        tree.root.rt.lt = new Node(6);
+        tree.root.rt.rt = new Node(7);
+
+        tree.DFS(tree.root);
+
+    }
+}
