72ee9292221db542f82b8994b8bdfeaf6e92becf
whitetigerlouis <82086525+whitetigerlouis@users.noreply.github.com>
2022-06-21 11:22:10 +0900

    Create 1546.java

---
+++ BAEKJOON/Basic/1546.java
@@ -0,0 +1,39 @@
+/*
+    [백준] 11720번 숫자의 합 구하기
+*/
+import java.util.*;
+
+public class Main {  
+    static int n;
+    static int[] A;
+    
+    public void inputData() {
+        Scanner in = new Scanner(System.in);
+        n = in.nextInt();
+        A = new int[n];
+        for (int i=0; i<n; i++) {
+            A[i] = in.nextInt();
+        }
+    }
+
+    public double solution() {
+        double answer = 0;
+        long max = 0;
+        long sum = 0;
+        for (int x : A) {
+            sum += x;
+            if (x > max) max = x;
+        }
+
+        answer = sum * 100.0 / max / n;
+        return answer;
+    }
+    
+    public static void main(String[] args) {
+        Main T = new Main();
+        T.inputData();
+        System.out.println(T.solution());
+        //System.out.println(answer);
+    }
+}
+
